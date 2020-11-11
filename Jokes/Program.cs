cf5e9c2ba2b13da57153218171ca107302ae2328
ok_alchemy <olyaalchemy@gmail.com>
2020-11-11 23:20:29 +0200

    Changed task

--- Jokes/Program.cs
+++ Jokes/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Mail;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -21,7 +23,7 @@ namespace Jokes
     }
     class SendMessage
     {
-        private const string URL = "https://api.eu.mailgun.net/v3/mg.brash.io/messages";
+        private const string URL = "https://disify.com/";
 
         static HttpClient client = new HttpClient();
 
@@ -29,11 +31,22 @@ namespace Jokes
         public static void Main1(string[] args)
         {
 
-            client.BaseAddress = new Uri(URL);
             dadJoke.BaseAddress = new Uri("https://icanhazdadjoke.com/");
             string joke = jetJoke().GetAwaiter().GetResult();
 
-            var result = SendJoke(joke).GetAwaiter().GetResult();
+            Console.WriteLine("Please enter email");
+            string addressTo = Console.ReadLine();
+            Console.WriteLine("Please enter password");
+            string pass = Console.ReadLine();
+
+            if (ValidateAdress(addressTo).GetAwaiter().GetResult())
+            {
+                var result = SendJoke(joke, addressTo, pass).GetAwaiter().GetResult();
+            }
+            else
+            {
+                Console.WriteLine("Email adress is not valid");
+            }
         }
 
         static async Task<string> jetJoke()
@@ -47,19 +60,8 @@ namespace Jokes
             return joke;
         }
 
-        static async Task<bool> SendJoke(string joke)
+        static async Task<bool> SendJoke(string joke, string addressTo, string pass)
         {
-           // client.DefaultRequestHeaders.Authorization = 
-           //     new AuthenticationHeaderValue("Basic", Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes("api" + ":" + "key-xxxxxxxxxxx")));
-
-            bool t = client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "key=xxxxxxx");
-
-            var form = new Dictionary<string, string>();
-
-            form["from"] = "<olyaalchemy@gmail.com>";
-            form["to"] = "<olyaalchemy@gmail.com>";
-            form["subject"] = "Dad joke test";
-
             string body;
             body = "<html dir='ltr' lang='en'>";
             body += "<head>";
@@ -71,13 +73,49 @@ namespace Jokes
             body += "</body>";
             body += "</html>";
 
-            form["html"] = Regex.Replace(body, @"\r\n?|\n", "<br />");
+            
 
-            var response = await client.PostAsync("https://api.mailgun.net/v2/" + "reach.softnames.com" + "/messages", new FormUrlEncodedContent(form));
+            SmtpClient smtpClient = new SmtpClient("glassix-hmail.westeurope.cloudapp.azure.com");
+            smtpClient.UseDefaultCredentials = false;
 
-            bool b = response.IsSuccessStatusCode;
+            NetworkCredential credential = new NetworkCredential("test@glassix-spam.com", pass);
+            smtpClient.Credentials = credential;
+
+
+            MailAddress addressFrom = new MailAddress("test@glassix-spam.com");
+            MailMessage message = new MailMessage();
+            message.From = addressFrom;
+            message.To.Add(addressTo);
+            message.Subject = "TestJoke";
+            message.IsBodyHtml = true;
+            message.Body = body;
+
+            try
+            {
+                smtpClient.Send(message);
+
+            }
+            catch (SmtpException ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+            }
 
             return true;
+        }
+
+        static async Task<bool> ValidateAdress(string adress)
+        {
+            client.BaseAddress = new Uri(URL);
+            HttpResponseMessage response = await client.GetAsync("api/email/" + adress);
+            string result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            Dictionary<string, object> o = new JavaScriptSerializer().Deserialize<Dictionary<string, object>>(result);
+            bool isAdressValid = (bool)o["format"];
+            return isAdressValid;
         }
     }
 }
